087cf02c24bee1b94c8f4f3d16de22735f99ca97
Tapoban Ray <raytapoban@gmail.com>
2025-03-01 10:26:16 +0530

    Created a books api
    Implemented GET, POST, PUT, DELETE requests

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# Learning FastAPI
+# Learning [FastAPI](https://fastapi.tiangolo.com/tutorial/#run-the-code)
 
-### [Learning Course.](https://youtu.be/0sOvCWFmrtA?si=m0TAHkn3qo-n7Ok3)
-### [YouTube Playlist](https://youtube.com/playlist?list=PLK8U0kF0E_D6l19LhOGWhVZ3sQ6ujJKq_&si=Ql6SZHEsI8XSJgzS)
+## Fist Steps
+1. Create a virtual environment using the following command on your terminal.
+    ```
+    python3 -m venv <venv_name>
+    ```
+2. 
+## Notes
+
+Fastapi is the framework and uvicorn is the ASGI (Asynchronous Server Gateway Interface) server that runs our application.
+#### HTTP Request Methods
+- GET -> READ DATA
+- POST -> CREATE DATA
+- PUT -> UPDATE DATA
+- DELETE -> DELETE DATA
+## Resources
+
+- [**YouTube Video Tutorial**](https://youtu.be/0sOvCWFmrtA?si=m0TAHkn3qo-n7Ok3)
+- [**YouTube Playlist**](https://youtube.com/playlist?list=PLK8U0kF0E_D6l19LhOGWhVZ3sQ6ujJKq_&si=Ql6SZHEsI8XSJgzS)
---
+++ books.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+app = FastAPI()
+
+# Model for a book
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=101)  # 0 - 100
+
+
+BOOKS = []
+
+# Reading all the books
+@app.get("/")
+def read_api():
+    return {"books": BOOKS}
+
+# Creating new book
+@app.post("/")
+def create_book(book: Book):
+    BOOKS.append(book)
+    return book
+
+# Updating an existing book
+@app.put("/{book_id}")
+def update_book(book_id: UUID, new_book: Book):
+    counter = 0
+    for book in BOOKS:
+        counter += 1
+        if book.id == book_id:
+            BOOKS[counter - 1] = new_book
+            return BOOKS[counter - 1]
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"ID {book_id}: Does not exist.",
+    )
+
+# Deleting an existing book
+@app.delete("/{book_id}")
+def delete_book(book_id: UUID):
+    counter = 0
+    for book in BOOKS:
+        counter += 1
+        if book.id == book_id:
+            del BOOKS[counter - 1]
+            return f"ID: {book_id} deleted"
+    raise HTTPException(
+        status_code=404,
+        detail=f"ID {book_id} does not exist.",
+    )
